933ec28bce64e2be3b1b2e3771863b3041313ad1
Irimurielle <irimurielle@gmail.com>
2020-11-01 23:22:01 +0200

    Auto-generated commit

--- src/app/past-time.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { PastTimePipe } from './past-time.pipe';
-
-describe('PastTimePipe', () => {
-  it('create an instance', () => {
-    const pipe = new PastTimePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/app/quote.ts
+++
@@ -1,4 +0,0 @@
-export class Quote {
-    constructor(public id: number,public publisher:string, public author:string,public name:string,public upvote: number,public downvote: number,public date: Date){
-    }
-}
