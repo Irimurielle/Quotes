f064d601df9808185b81425a538e54ae1da20b4d
Irimurielle <irimurielle@gmail.com>
2020-11-01 15:09:10 +0200

    created a constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,4 @@
 export class Quote {
+    constructor(public id: number,public name:string, public author:string,public quote:string,public upvote: number,public downvote: number,public date: Date){
+    }
 }
